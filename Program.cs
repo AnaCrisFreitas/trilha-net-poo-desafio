c3cc807be2b2cf938cc8d75efae2405f42836033
AnaCrisFreitas <anacrisfreitas@gmail.com>
2024-01-08 23:05:07 -0300

    modificações
    realizado modificações no código conforme solicitado em projeto

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone Iphone3s = new("21976533", "3329101", 32);
+Iphone3s.InstalarAplicativo("Whatsapp");
+
+Nokia MotoE20 = new("2197645324", "3214795", 181);
+MotoE20.InstalarAplicativo("Minecarft");
